8f4a9dde7d7b512cdcd04a5b6624bce64f231ee2
InMotion ARM Roderick <roderick@android-2d1a33fa6488ca46.robots.local>
2019-11-18 20:04:35 -0500

    regular update

--- InMotionPyCtrl/roderick_crob/movebox.py
+++ InMotionPyCtrl/roderick_crob/movebox.py
@@ -7,6 +7,7 @@
 
 import ShmPyCtrl.ShmPy as shm
 import subprocess
+import time
 
 shm_path = './shm'
 
@@ -24,7 +25,7 @@ def start_rtl():
     subprocess.run(['tclsh ./start.tcl'], shell=True)
 
 def wshm(target, value):
-    shm.send_command(get_command(action='set', target=target, value=value, path=shm_path))
+    shm.send_command(shm.get_command(action='set', target=target, value=value, path=shm_path))
 
 def set_default():
     wshm('no_safety_check', 1)
@@ -42,12 +43,19 @@ def set_command(direction, speed, start_pos, end_pos):
     command['end_pos'] = end_pos
 
 def run_movebox():
-    subprocess.run(['tclsh ./movebox.tcl '+command['direction']+' '+command['speed']+' '\
-        +command['start_pos'][0]+' '+command['start_pos'][1]+' '+command['end_pos'][0]+' '\
-        +command['end_pos'][1]], shell=Ture)
+    subprocess.run(['tclsh ./movebox.tcl '+str(command['direction'])+' '+str(command['speed'])+' '\
+        +str(command['start_pos'][0])+' '+str(command['start_pos'][1])+' '+str(command['end_pos'][0])+' '\
+        +str(command['end_pos'][1])], shell=True)
+#    wshm('slot0_go', 1)
 
 if __name__ == '__main__':
     start_rtl()
-    set_default()
-    set_command('left', 0.05, (0, 0), (0.3, 0))
+#    set_default()
+    set_command('left', 0.05, (0, 0), (0.1, 0))
+    run_movebox()
+    time.sleep(5)
+    set_command('left', 0.05, (0.1, 0), (-0.2, -0.2))
+    run_movebox()
+    time.sleep(5)
+    set_command('left', 0.05, (-0.2, -0.2), (-0.1, 0.2))
     run_movebox()
--- InMotionPyCtrl/roderick_crob/movebox.tcl
+++ InMotionPyCtrl/roderick_crob/movebox.tcl
@@ -3,7 +3,21 @@
 # @Email   : roderick_wang@outlook.com
 # @GitHub  : https://github.com/hello-roderickwang
 
-source ./shm.tcl
+global ob
+
+set ob(crobhome) $::env(CROB_HOME)
+
+source $ob(crobhome)/shm.tcl
+
+#start_lkm
+start_shm
+start_loop
+after 100
+
+wshm no_safety_check 1
+wshm stiff 100
+wshm damp 5
+wshm slot_max 4
 
 set direction "null"
 set speed 0
@@ -16,7 +30,7 @@ set end_y 0
 # if you want this movebox command refresh every 5 seconds, set it to 200*5=1000
 set ticks 1000
 
-if {$argc != 6}{
+if {$argc != 6} {
     puts "The movebox.tcl script requires 6 arguments."
     puts "Arguments are:"
     puts "    direction(up/down/left/right)"
@@ -25,7 +39,7 @@ if {$argc != 6}{
     puts "    start_y(float)"
     puts "    end_x(float)"
     puts "    end_y(float)"
-}else{
+} else {
     set direction [lindex $argv 0]
     set speed [lindex $argv 1]
     set start_x [lindex $argv 2]
@@ -34,4 +48,13 @@ if {$argc != 6}{
     set end_y [lindex $argv 5]
 }
 
+puts $direction
+puts $speed
+puts $start_x
+puts $start_y
+puts $end_x
+puts $end_y
+
 movebox 0 0 {0 $ticks 1} {$start_x $start_y 0 0} {$end_x $end_y 0 0}
+
+# after 5000
